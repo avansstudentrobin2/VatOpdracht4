b865d3a2ebe9b61f13f4cf698fdfa88dc49f66eb
Robin <rh.tanis@avans.nl>
2018-10-25 16:09:05 +0200

    Last changes 1.1

--- src/frontend/Controller.java
+++ src/frontend/Controller.java
@@ -1,4 +1,5 @@
 package frontend;
+
 import classes.GetAllShapes;
 import classes.GetVolume;
 import javafx.application.Platform;
@@ -17,7 +18,6 @@ import Shape.Shape;
 import java.awt.*;
 import java.net.URL;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -32,8 +32,11 @@ public class Controller implements Initializable {
     private TableColumn<Shape, String> shapenameColumn;
     @FXML
     private TableColumn<Shape, Double> parametersColumn;
+
     @FXML
-    private void displayValue(ActionEvent event) {}
+    private void displayValue(ActionEvent event) {
+    }
+
     @FXML
     private ChoiceBox choiceBox;
     private String selectedShape = "Bol";
@@ -46,13 +49,13 @@ public class Controller implements Initializable {
     public Controller() {
         try {
             GetAllShapes = new GetAllShapes();
-        } catch ( Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
         shapenameColumn.setCellValueFactory(new PropertyValueFactory<>("shapeName"));
         selectedShapeVolume.setText("Selecteer een vorm");
         choiceBox.getItems().add("Bol");
@@ -61,32 +64,29 @@ public class Controller implements Initializable {
         choiceBox.getItems().add("Kegel");
         choiceBox.getItems().add("Piramide");
         choiceBox.setValue("Bol");
-        try{
+        try {
             tableViewShapes.setItems(GetAllShapes.getAllShapes());
             getTotalShapeVolume();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
+
     @FXML
     public void getTotalShapeVolume() {
         double totalVolume = 0;
 
         ObservableList<Shape> allShapes = tableViewShapes.getItems();
-        for(Shape shape : allShapes) {
-            if(shape.getShapeName().equals("Bol")){
+        for (Shape shape : allShapes) {
+            if (shape.getShapeName().equals("Bol")) {
                 totalVolume += getVolume.volumeBol(shape);
-            }
-            else if(shape.getShapeName().equals("Cilinder")) {
+            } else if (shape.getShapeName().equals("Cilinder")) {
                 totalVolume += getVolume.volumeCilinder(shape);
-            }
-            else if(shape.getShapeName().equals("Blok")) {
+            } else if (shape.getShapeName().equals("Blok")) {
                 totalVolume += getVolume.volumeBlok(shape);
-            }
-            else if(shape.getShapeName().equals("Kegel")) {
+            } else if (shape.getShapeName().equals("Kegel")) {
                 totalVolume += getVolume.volumeKegel(shape);
-            }
-            else if(shape.getShapeName().equals("Piramide")) {
+            } else if (shape.getShapeName().equals("Piramide")) {
                 totalVolume += getVolume.volumePiramide(shape);
             }
         }
@@ -97,21 +97,21 @@ public class Controller implements Initializable {
     }
 
     public void newShape(ActionEvent event) {
-        if(choiceBox.getValue().toString().equals("Bol")) {
+        if (choiceBox.getValue().toString().equals("Bol")) {
             SceneChanger sc = new SceneChanger();
-            sc.changeScenes(event,"bolcontroller.fxml", "Add Bol");
-        } else if(choiceBox.getValue().toString().equals("Cilinder")) {
+            sc.changeScenes(event, "bolcontroller.fxml", "Add Bol");
+        } else if (choiceBox.getValue().toString().equals("Cilinder")) {
             SceneChanger sc = new SceneChanger();
-            sc.changeScenes(event,"cilindercontroller.fxml", "Add Cilinder");
-        } else if(choiceBox.getValue().toString().equals("Blok")) {
+            sc.changeScenes(event, "cilindercontroller.fxml", "Add Cilinder");
+        } else if (choiceBox.getValue().toString().equals("Blok")) {
             SceneChanger sc = new SceneChanger();
-            sc.changeScenes(event,"blokcontroller.fxml", "Add Blok");
-        } else if(choiceBox.getValue().toString().equals("Kegel")) {
+            sc.changeScenes(event, "blokcontroller.fxml", "Add Blok");
+        } else if (choiceBox.getValue().toString().equals("Kegel")) {
             SceneChanger sc = new SceneChanger();
-            sc.changeScenes(event,"kegelcontroller.fxml", "Add Kegel");
-        } else if(choiceBox.getValue().toString().equals("Piramide")) {
+            sc.changeScenes(event, "kegelcontroller.fxml", "Add Kegel");
+        } else if (choiceBox.getValue().toString().equals("Piramide")) {
             SceneChanger sc = new SceneChanger();
-            sc.changeScenes(event,"piramidecontroller.fxml", "Add Piramide");
+            sc.changeScenes(event, "piramidecontroller.fxml", "Add Piramide");
         }
     }
 
@@ -126,7 +126,7 @@ public class Controller implements Initializable {
             alert.show();
             tableViewShapes.setItems(GetAllShapes.getAllShapes());
             getTotalShapeVolume();
-        } catch(Exception e){
+        } catch (Exception e) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Verwijder fout");
             alert.setHeaderText(null);
@@ -135,33 +135,31 @@ public class Controller implements Initializable {
         }
 
     }
+
     public void closeProgram() {
         Platform.exit();
     }
+
     public void choiceBoxChanged() {
         selectedShape = choiceBox.getValue().toString();
         getTotalShapeVolume();
     }
+
     public void tableViewNewSelected() {
-        System.out.println("New selected");
         Shape selectedShape = tableViewShapes.getSelectionModel().getSelectedItem();
-        if(selectedShape != null){
-            if(selectedShape.getShapeName().equals("Bol")){
+        if (selectedShape != null) {
+            if (selectedShape.getShapeName().equals("Bol")) {
                 selectedShapeVolume.setText(Double.toString(getVolume.volumeBol(selectedShape)));
-            }
-            else if(selectedShape.getShapeName().equals("Cilinder")) {
+            } else if (selectedShape.getShapeName().equals("Cilinder")) {
                 selectedShapeVolume.setText(Double.toString(getVolume.volumeCilinder(selectedShape)));
-            }
-            else if(selectedShape.getShapeName().equals("Blok")) {
+            } else if (selectedShape.getShapeName().equals("Blok")) {
                 selectedShapeVolume.setText(Double.toString(getVolume.volumeBlok(selectedShape)));
-            }
-            else if(selectedShape.getShapeName().equals("Kegel")) {
+            } else if (selectedShape.getShapeName().equals("Kegel")) {
                 selectedShapeVolume.setText(Double.toString(getVolume.volumeKegel(selectedShape)));
-            }
-            else if(selectedShape.getShapeName().equals("Piramide")) {
+            } else if (selectedShape.getShapeName().equals("Piramide")) {
                 selectedShapeVolume.setText(Double.toString(getVolume.volumePiramide(selectedShape)));
             }
-        } else{
+        } else {
             selectedShapeVolume.setText("Selecteer een vorm");
         }
     }
